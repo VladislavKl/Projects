40fe738f6e00798618cf8d41229a050c486c58d6
VladislavKl <vladkld13@gmail.com>
2019-02-22 23:07:32 +0300

    Add files via upload

---
+++ CLion/Assembler/cmake-build-debug/CMakeFiles/Assembler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assembler.dir/main.cpp.o"
+  "Assembler.pdb"
+  "Assembler"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assembler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

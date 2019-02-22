c731fc0e8e279d6506bbf2728e74940b24821e28
VladislavKl <vladkld13@gmail.com>
2019-02-22 23:05:31 +0300

    Add files via upload

---
+++ CLion/28_Timetable_Structures/cmake-build-debug/CMakeFiles/28_Timetable_Structures.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/28_Timetable_Structures.dir/main.cpp.o"
+  "28_Timetable_Structures.pdb"
+  "28_Timetable_Structures"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/28_Timetable_Structures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
